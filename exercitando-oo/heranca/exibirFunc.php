0317e8fb5b10793157c29fcce8cb66c5fddd4163
rlcarneiro97 <rafaeldelima97@hotmail.com>
2020-10-23 21:59:50 -0300

    completando atividade

--- exercitando-oo/heranca/exibirFunc.php
+++ exercitando-oo/heranca/exibirFunc.php
@@ -1,8 +1,24 @@
 <?php
 
-    echo "<p>{$_POST['nome']}</p>";
-    echo "<p>{$_POST['salario']}</p>";
-    echo "<p>{$_POST['observacoes']}</p>";
-    echo "<p>{$_POST['profissao']}</p>";
+    require_once "classes/Funcionario.php";
+    require_once "classes/Programador.php";
+    require_once "classes/Gerente.php";
+
+    $nome = $_POST['nome'];
+    $salario = $_POST['salario'];
+    $observacao = $_POST['observacao'];
+    $cargo = $_POST['cargo'];
+
+    // echo "<p>{$nome}</p><p>{$salario}</p><p>{$observacao}</p><p>{$cargo}</p>";
+
+    if($cargo === "gerente")
+    {
+        $gerente = new Gerente($nome, $salario, $observacao);
+    }
+    
+    if($cargo === "programador")
+    {
+        $programador = new Programador($nome, $salario, $observacao);
+    }
 
 ?>
